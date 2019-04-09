5d7ac5937fc9dc45c3862e93c1e39d65e33d1026
anlmgp <anitalopezmarotog@gmail.com>
2019-04-09 17:11:07 +0200

    Basis level

---
+++ server.py
@@ -0,0 +1,67 @@
+import http.server
+import socketserver
+import termcolor
+import requests, sys
+
+server = "http://rest.ensembl.org"
+ext = "/info/species?"
+r = requests.get(server + ext, headers={"Content-Type": "application/json"})
+if not r.ok:
+    r.raise_for_status()
+    sys.exit()
+decoded = r.json()
+name = decoded['species']
+name = name[1]
+print(name)
+
+PORT = 8000
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+
+
+    def do_GET(self):
+        import requests, sys
+        server = "http://rest.ensembl.org"
+        ext = "/info/species?"
+        r = requests.get(server + ext, headers={"Content-Type": "application/json"})
+        if not r.ok:
+            r.raise_for_status()
+            sys.exit()
+        decoded = r.json()
+        name = repr(decoded)
+        name = name('species')
+        print(name)
+
+
+        if self.path == '/' or self.path == '/favicon.ico':
+            f = open("form.html", 'r')
+            contents = f.read()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Lenght', len(str.encode(contents)))
+            self.end_headers()
+
+        #-- SEnding the body of the response message
+            self.wfile.write(str.encode(contents))
+
+        else:
+            s = open("error.html")
+            contents = s.read()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Lenght', len(str.encode(contents)))
+            self.end_headers()
+            # -- SEnding the body of the response message
+            self.wfile.write(str.encode(contents))
+
+# -- Main program
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+    print("Serving at PORT: {}".format(PORT))
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+print("The server is closed")

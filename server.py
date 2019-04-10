6cec6fdd57df4b8b529230d358616538ca708332
anlmgp <anitalopezmarotog@gmail.com>
2019-04-10 19:09:14 +0200

    Basis level

--- server.py
+++ server.py
@@ -1,52 +1,45 @@
 import http.server
 import socketserver
 import termcolor
-
 import requests, sys
-server = "http://rest.ensembl.org"
-ext = "/info/species?"
-r = requests.get(server + ext, headers={"Content-Type": "application/json"})
-if not r.ok:
-    r.raise_for_status()
-    sys.exit()
-decoded = r.json()
-name = decoded['species']
-for i in name rang:
-    n =i['common_name']
-    print(n)
-
-print (len(n))
 
 
 PORT = 8000
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
+    server = "http://rest.ensembl.org"
+    ext = "/info/species?"
+    r = requests.get(server + ext, headers={"Content-Type": "application/json"})
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
+    decoded = r.json()
+    name = decoded['species']
+    for i in name:
+        n = i['common_name']
 
 
 
     def do_GET(self):
+        print(self.path)
 
-
-        if self.path == '/' or self.path == '/favicon.ico':
+        if self.path == '/':
             f = open("form.html", 'r')
             contents = f.read()
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Lenght', len(str.encode(contents)))
-            self.end_headers()
 
-        #-- SEnding the body of the response message
-            self.wfile.write(str.encode(contents))
+        elif self.path.startswith( '/listSpecies?limit='):
+            pass
 
         else:
             s = open("error.html")
             contents = s.read()
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Lenght', len(str.encode(contents)))
-            self.end_headers()
-            # -- SEnding the body of the response message
-            self.wfile.write(str.encode(contents))
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Lenght', len(str.encode(contents)))
+        self.end_headers()
+        # -- SEnding the body of the response message
+        self.wfile.write(str.encode(contents))
 
 # -- Main program
 with socketserver.TCPServer(("", PORT), TestHandler) as httpd:

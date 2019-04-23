08c5dee90938c90a4fdbf8ffb2961a588081faa0
anlmgp <anitalopezmarotog@gmail.com>
2019-04-23 15:06:03 +0200

    /lisspecies

--- server.py
+++ server.py
@@ -3,32 +3,68 @@ import socketserver
 import termcolor
 import requests, sys
 
-
 PORT = 8000
 
+
+server = "http://rest.ensembl.org"
+ext = "/info/species?"
+r = requests.get(server + ext, headers={"Content-Type": "application/json"})
+if not r.ok:
+    r.raise_for_status()
+    sys.exit()
+decoded = r.json()
+name = decoded['species']
+result1 = []
+result= ''
+for i in name:
+    n = str(i['common_name'])
+    result1.append(n)
+    result += "<p>{}</p>".format(n)
+
+
+
 class TestHandler(http.server.BaseHTTPRequestHandler):
-    server = "http://rest.ensembl.org"
-    ext = "/info/species?"
-    r = requests.get(server + ext, headers={"Content-Type": "application/json"})
-    if not r.ok:
-        r.raise_for_status()
-        sys.exit()
-    decoded = r.json()
-    name = decoded['species']
-    for i in name:
-        n = i['common_name']
-
-
 
     def do_GET(self):
         print(self.path)
 
+        species = self.path.split ('?')
+        total_species = species[0]
+        if len(species)== 2:
+            number = species[1]
+            number1 =number.split ('=')
+            if len(number1) == 2:
+                number2 =number1[1]
+
+
         if self.path == '/':
             f = open("form.html", 'r')
             contents = f.read()
-
-        elif self.path.startswith( '/listSpecies?limit='):
-            pass
+        elif total_species == '/listSpecies':
+            if number.endswith('='):
+                contents = """<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                        <meta charset="UTF-8">
+                        <title>RESULT</title>
+                        </head>
+                        <body>
+                        <h1>List of species</h1>
+                        {}</p>
+                        <a href="/">Link to main</a>""".format(result)
+            else:
+                resultt = result1[0:int(number2)]
+                contents = """<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                        <meta charset="UTF-8">
+                        <title>RESULT</title>
+                        </head>
+                        <body>
+                        <h1>List of species</h1>
+                        {}</p>
+                        <a href="/">Link to main</a>""".format(resultt)
+        elif 
 
         else:
             s = open("error.html")

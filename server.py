bb773a8c67cbf48c6b514c3893bbde5f97a1e25c
anlmgp <anitalopezmarotog@gmail.com>
2019-04-29 22:50:01 +0200

    Basic Level finish

--- server.py
+++ server.py
@@ -1,75 +1,80 @@
 import http.server
 import socketserver
-import termcolor
-import requests, sys
+import sys
+import requests
 
 PORT = 8000
 
 
-server = "http://rest.ensembl.org"
-ext = "/info/species?"
-r = requests.get(server + ext, headers={"Content-Type": "application/json"})
-if not r.ok:
-    r.raise_for_status()
-    sys.exit()
-decoded = r.json()
-name = decoded['species']
-result1 = []
-result= ''
-for i in name:
-    n = str(i['common_name'])
-    result1.append(n)
-    result += "<p>{}</p>".format(n)
-
-def karotype(specie):
+def listpecies():
     server = "http://rest.ensembl.org"
-    ext = "/info/assembly/"+ specie
+    ext = "/info/species?"
     r = requests.get(server + ext, headers={"Content-Type": "application/json"})
     if not r.ok:
-        result = 'requests.exceptions.HTTPError: 400 Client Error: Bad Request for url: http://rest.ensembl.org/info/assembly/a'
+        r.raise_for_status()
+        sys.exit()
+    decoded = r.json()
+    name = decoded['species']
+    result1 = []
+    result = ''
+    for i in name:
+        n = str(i['name'])
+        result1.append(n)
+        result += "<p>{}</p>".format(n)
+    return result, result1
+
+
+def karyotype(specie):
+    server = "http://rest.ensembl.org"
+    ext = "/info/assembly/" + specie
+    r = requests.get(server + ext, headers={"Content-Type": "application/json"})
+    if not r.ok:
+        result = """requests.exceptions.HTTPError: 400 
+        Client Error: Bad Request for url: http://rest.ensembl.org/info/assembly/""" + specie
         return result
     decoded = r.json()
     result = decoded['karyotype']
     return result
 
 
-def lenght(specie, chromo):
+def length(specie, chromo):
     server = "http://rest.ensembl.org"
-    ext = "/info/assembly/"+ specie + "/" +chromo
+    ext = "/info/assembly/" + specie + "/" + chromo
     r = requests.get(server+ext, headers={"Content-Type": "application/json"})
     if not r.ok:
-        result = 'requests.exceptions.HTTPError: 400 Client Error: Bad Request for url: http://rest.ensembl.org/info/assembly/'+ext
+        result = """requests.exceptions.HTTPError: 400 Client Error: Bad Request for url: http://rest.ensembl.org"""+ext
         return result
     decoded = r.json()
-    length = decoded['length']
-    return length
-
+    length1 = decoded['length']
+    return length1
 
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
+        global spe, chromosome, number2
         print(self.path)
 
-        endpoint1 =''
-        l_endpoint = self.path.split ('?')
+        endpoint1 = ''
+        l_endpoint = self.path.split('?')
         endpoint = l_endpoint[0]
-        if len(l_endpoint)== 2:
+        if len(l_endpoint) == 2:
             endpoint1 += str(l_endpoint[1])
-            number1 =endpoint1.split ('=')
+            number1 = endpoint1.split('=')
             if len(number1) == 2:
-                number2 =number1[1]
+                number2 = number1[1]
             number = endpoint1.split('&')
-            if len (number) == 2:
-                spe =  number[0].lstrip('specie=')
+            if len(number) == 2:
+                spe = number[0].lstrip('specie=')
                 chromosome = number[1].lstrip('chromo=')
-
+                print (spe, chromosome)
 
         if self.path == '/':
             f = open("form.html", 'r')
             contents = f.read()
 
         elif endpoint == '/listSpecies' or (self.path == '/listSpecies'):
+            result, result1 = listpecies()
             if endpoint1.endswith('=') or (self.path == '/listSpecies'):
                 contents = """<!DOCTYPE html>
                         <html lang="en">
@@ -110,10 +115,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         <body>
                         <h1>Karyotype of {}</h1>
                         {}</p>
-                        <a href="/">Link to main</a>""".format(number2,karotype(number2))
+                        <a href="/">Link to main</a>""".format(number2, karyotype(number2))
 
         elif endpoint == '/chromosomeLength':
-            contents = """<!DOCTYPE html>
+            if chromosome != '' and spe != '':
+                contents = """<!DOCTYPE html>
                     <html lang="en">
                     <head>
                     <meta charset="UTF-8">
@@ -122,7 +128,10 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     <body>
                     <h1>Lenght of {} chromosome of the specie {}. </h1>
                     The lenght is: {}</p>
-                    <a href="/">Link to main</a>""".format(chromosome,spe, lenght(spe,chromosome))
+                    <a href="/">Link to main</a>""".format(chromosome, spe, length(spe, chromosome))
+            else:
+                s = open("error.html")
+                contents = s.read()
 
         else:
             s = open("error.html")
@@ -136,9 +145,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(str.encode(contents))
 
 # -- Main program
-with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
-    print("Serving at PORT: {}".format(PORT))
 
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+
+    print("Serving at PORT: {}".format(PORT))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
